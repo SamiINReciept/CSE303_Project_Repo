f568e6b4e3c642f2cba4426eb42bf8be273ea2ba
Faria-Anjum <114348170+Faria-Anjum@users.noreply.github.com>
2022-11-30 23:04:26 +0600

    linked pages in django

--- Django/spmsMain/spms3/urls.py
+++ Django/spmsMain/spms3/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 from spms3 import views
 
 urlpatterns = [
-    path('', views.home, name='homepage'),
-    path('signin', views.login, name='sign-in')
+    path('', views.login, name='sign-in'),
+    path('sign_in', views.login, name='sign-in'),
+    path('faculty/dashboard', views.fDashboard, name='faculty-dashboard'),
+    path('faculty/co', views.fCo, name='faculty-co'),
+    path('faculty/qb', views.fQb, name='faculty-qb'),
+    path('createqb', views.createQb, name='create-qb'),
+    path('createco', views.createCo, name='create-co'),
+
 ]
--- Django/spmsMain/spms3/views.py
+++ Django/spmsMain/spms3/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'homepage.html')
-
 def login(request):
-    return render(request, 'signin.html')
+    return render(request, 'signin/sign_in.html')
+
+def fDashboard(request):
+    return render(request, 'dashboard/faculty/faculty_dashboard.html')
+
+def fCo(request):
+    return render(request, 'dashboard/faculty/faculty_CO.html')
+
+def fQb(request):
+    return render(request, 'dashboard/faculty/faculty_QB.html')
+
+def createQb(request):
+    return render(request, 'dashboard/faculty/questionbank/createQB.html')
+
+def createCo(request):
+    return render(request, 'dashboard/faculty/courseoutline/createCO.html')
 

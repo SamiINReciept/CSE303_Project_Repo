a1e8b555702abda5c2754683403192ef4dd8fc9a
Faria-Anjum <114348170+Faria-Anjum@users.noreply.github.com>
2022-12-01 18:15:50 +0600

    created models

--- Django/spmsMain/spms3/admin.py
+++ Django/spmsMain/spms3/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(School_T)
+admin.site.register(Department_T)
+#admin.site.register(Employee_T)
+admin.site.register(Dean_T)
+admin.site.register(Head_T)
+admin.site.register(Faculty_T)
+
+
+
+
--- Django/spmsMain/spms3/models.py
+++ Django/spmsMain/spms3/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class School_T(models.Model):
+    schoolID = models.CharField(max_length=5, primary_key=True)
+    schoolName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.schoolID
+
+
+class Department_T(models.Model):
+    departmentID = models.CharField(max_length=5, primary_key=True)
+    departmentName = models.CharField(max_length=50)
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.departmentID
+
+
+class Employee_T(models.Model):
+    employeeID = models.CharField(max_length=4, primary_key=True)
+    employeeName = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=30, null=True)
+    employeeType = models.CharField(max_length=1, null=True)
+
+    class Meta:
+            abstract = True
+
+class Dean_T(Employee_T):
+    #deanID = models.CharField(max_length=4, primary_key=True)
+    startDate = models.CharField(max_length=15, default='N/A')
+    endDate = models.CharField(max_length=15, default='N/A')
+    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+
+class Head_T(Employee_T):
+    #headID = models.CharField(max_length=4, primary_key=True)
+    startDate = models.CharField(max_length=15,default='N/A')
+    endDate = models.CharField(max_length=15,default='N/A')
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+
+
+class Faculty_T(Employee_T):
+    #facultyID = models.IntegerField(primary_key=True)
+    joinDate = models.DateField(null=True)
+    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.firstName + " "+ self.lastName
+
+
+
+

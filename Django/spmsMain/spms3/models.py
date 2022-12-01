c244ac1ffbe974d06e1f4263fd2a7dd663d9a15e
Sumaiya-Karim-Katha <114346589+Sumaiya-Karim-Katha@users.noreply.github.com>
2022-12-01 21:43:42 +0600

    Update models.py

--- Django/spmsMain/spms3/models.py
+++ Django/spmsMain/spms3/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class School_T(models.Model):
-    schoolID = models.CharField(max_length=5, primary_key=True)
-    schoolName = models.CharField(max_length=50)
+class SCHOOL_T(models.Model):
+    SchoolID = models.CharField(max_length=5, primary_key=True)
+    SchoolName = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.schoolID
+        return self.SchoolID
 
 
 class Department_T(models.Model):
     departmentID = models.CharField(max_length=5, primary_key=True)
     departmentName = models.CharField(max_length=50)
-    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+    school = models.ForeignKey(SCHOOL_T, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.departmentID

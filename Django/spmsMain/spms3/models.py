53a83526898c4244f2c5b2919a53eb4bff4f12e7
Sumaiya-Karim-Katha <114346589+Sumaiya-Karim-Katha@users.noreply.github.com>
2022-12-02 02:22:17 +0600

    Update models.py

--- Django/spmsMain/spms3/models.py
+++ Django/spmsMain/spms3/models.py
@@ -2,41 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-class School_T(models.Model):
-    schoolID = models.CharField(max_length=5, primary_key=True)
-    schoolName = models.CharField(max_length=50)
+class SCHOOL_T(models.Model):
+    SchoolID = models.CharField(max_length=5, primary_key=True)
+    SchoolName = models.CharField(max_length=50)
 
     def __str__(self):
         return self.SchoolID
 
 
-class Department_T(models.Model):
-    departmentID = models.CharField(max_length=5, primary_key=True)
-    departmentName = models.CharField(max_length=50)
-    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+class DEPARTMENT_T(models.Model):
+    DepartmentID = models.CharField(max_length=5, primary_key=True)
+    DepartmentName = models.CharField(max_length=50)
+    SchoolID = models.ForeignKey(SCHOOL_T, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.departmentID
+        return self.DepartmentID
 
 
-class Program_T(models.Model):
-    programID = models.AutoField(primary_key=True)
-    programName = models.CharField(max_length=70)
-    department = models.ForeignKey(Department_T, on_delete=models.CASCADE, default='N/A')
+class PROGRAM_T(models.Model):
+    ProgramID = models.AutoField(primary_key=True)
+    ProgramName = models.CharField(max_length=70)
+    DepartmentID = models.ForeignKey(DEPARTMENT_T, on_delete=models.CASCADE, default='N/A')
 
     def __str__(self):
         return self.programName
 
-class Student_T(models.Model):
-    studentID = models.CharField(max_length=7, primary_key=True)
-    Name = models.CharField(max_length=50, null=True)
-    email = models.CharField(max_length=50, null=True)
-    enrollmentDate = models.DateField(null=True)
-    #program = models.ForeignKey(Program_T, on_delete=models.CASCADE, default='N/A')
-    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+class STUDENT_T(models.Model):
+    StudentID = models.CharField(max_length=7, primary_key=True)
+    StudentName = models.CharField(max_length=50, null=True)
+    Email = models.CharField(max_length=50, null=True)
+    ProgramID = models.CharField(max_length=50, null=True)
 
     def __str__(self):
-        return self.studentID
+        return self.StudentID
 
 
 class Employee_T(models.Model):
@@ -52,19 +50,19 @@ class Dean_T(Employee_T):
     #deanID = models.CharField(max_length=4, primary_key=True)
     startDate = models.CharField(max_length=15, default='N/A')
     endDate = models.CharField(max_length=15, default='N/A')
-    school = models.ForeignKey(School_T, on_delete=models.CASCADE)
+    school = models.ForeignKey(SCHOOL_T, on_delete=models.CASCADE)
 
 class Head_T(Employee_T):
     #headID = models.CharField(max_length=4, primary_key=True)
     startDate = models.CharField(max_length=15,default='N/A')
     endDate = models.CharField(max_length=15,default='N/A')
-    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+    department = models.ForeignKey(DEPARTMENT_T, on_delete=models.CASCADE)
 
 
 class Faculty_T(Employee_T):
     #facultyID = models.IntegerField(primary_key=True)
     joinDate = models.DateField(null=True)
-    department = models.ForeignKey(Department_T, on_delete=models.CASCADE)
+    department = models.ForeignKey(DEPARTMENT_T, on_delete=models.CASCADE)
 
     def __str__(self):
        return self.firstName + " "+ self.lastName
@@ -73,7 +71,7 @@ class Course_T(models.Model):
     courseID = models.CharField(max_length=7, primary_key=True)
     courseName = models.CharField(max_length=50, null=True)
     numOfCredits = models.DecimalField(max_digits=2, decimal_places=1)
-    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    program = models.ForeignKey(PROGRAM_T, on_delete=models.CASCADE)
     courseType = models.CharField(max_length=15)
 
     def __str__(self):
@@ -83,7 +81,7 @@ class PLO_T(models.Model):
     ploID = models.AutoField(primary_key=True)
     ploNum = models.CharField(max_length=5)
     details = models.CharField(max_length=50)
-    program = models.ForeignKey(Program_T, on_delete=models.CASCADE)
+    program = models.ForeignKey(PROGRAM_T, on_delete=models.CASCADE)
    
 
     def __str__(self):

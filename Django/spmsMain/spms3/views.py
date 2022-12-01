0d8c50e060a326c9ab29ed12540ba6dc81f3cc16
Faria-Anjum <114348170+Faria-Anjum@users.noreply.github.com>
2022-12-01 16:29:52 +0600

    made migrations

--- Django/spmsMain/spms3/views.py
+++ Django/spmsMain/spms3/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+#from django.contrib.auth import authenticate, login, logout
+#from django.contrib import messages
 
 # Create your views here.
 

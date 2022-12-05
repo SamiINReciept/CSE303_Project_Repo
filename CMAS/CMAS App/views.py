88eb06ebee46bbab68a6ad59514342660f4c72e2
Ezharuddin-Jubaer <2120117@iub.edu.bd>
2022-12-05 20:17:35 +0600

    starting from scratch

---
+++ CMAS/CMAS App/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        utype = (request.POST.get('usertype'))
+        if utype == "faculty":
+            return render(request, 'dashboard/faculty/faculty_dashboard.html')
+        else:
+            return render(request, 'dashboard/student/student_dashboard.html')
+
+    return render(request, 'signin/sign_in.html')
+
+
+def fDashboard(request):
+    return render(request, 'dashboard/faculty/faculty_dashboard.html')
+
+def fCo(request):
+    return render(request, 'dashboard/faculty/faculty_CO.html')
+
+def fQb(request):
+    return render(request, 'dashboard/faculty/faculty_QB.html')
+
+def sDashboard(request):
+    return render(request, 'dashboard/student/student_dashboard.html')
+
+def sCo(request):
+    return render(request, 'dashboard/student/student_CO.html')
+
+def sQb(request):
+    return render(request, 'dashboard/student/student_QB.html')
+
+def createQb(request):
+    return render(request, 'dashboard/faculty/questionbank/createQB.html')
+
+def viewCo(request):
+    return render(request, 'dashboard/faculty/courseoutline/ViewCO.html')
+
+def createCo(request):
+    return render(request, 'dashboard/faculty/courseoutline/createCO.html')
+
+def viewAssessment(request):
+    return render(request, 'dashboard/faculty/questionbank/viewAssessment.html')
+
+

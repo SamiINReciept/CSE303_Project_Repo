b1aca8b952bb23f38008fa47f6a484ab91353404
Ezharuddin-Jubaer <2120117@iub.edu.bd>
2022-12-06 00:12:04 +0600

    update

--- DBMS/CMAS/models.py
+++ DBMS/CMAS/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-class user(AbstractUser):
-    class role(models.TextChoices):
-        admin = "admin", 'admin'
-        faculty = "faculty", 'faculty'
-        student = "student", 'student'
+# class user(AbstractUser):
+#     class role(models.TextChoices):
+#         admin = "admin", 'admin'
+#         faculty = "faculty", 'faculty'
+#         student = "student", 'student'
 
-    base_role = role.admin
+#     base_role = role.admin
 
-    role = models.CharField(max_length='50', choices=role.choices)
+#     role = models.CharField(max_length='50', choices=role.choices)
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.role = self.base_role
-            return super().save(*args, **kwargs)
+#     def save(self, *args, **kwargs):
+#         if not self.pk:
+#             self.role = self.base_role
+#             return super().save(*args, **kwargs)
--- DBMS/DBMS/settings.py
+++ DBMS/DBMS/settings.py
@@ -130,5 +130,5 @@ STATICFILES_DIRS = [BASE_DIR /'static',]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = "user.user"
+# AUTH_USER_MODEL = "user.user"
 

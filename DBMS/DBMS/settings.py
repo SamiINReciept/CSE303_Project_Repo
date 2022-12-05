85b2007c3e815fca1dc7e8c5a64f35ba5c63fbb6
Sumaiya-Karim-Katha <114346589+Sumaiya-Karim-Katha@users.noreply.github.com>
2022-12-05 23:42:25 +0600

    change

--- DBMS/DBMS/settings.py
+++ DBMS/DBMS/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DBMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbms',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'cockroach',
     }
 }
 
@@ -124,5 +128,3 @@ STATICFILES_DIRS = [BASE_DIR /'static',]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = "user.User"

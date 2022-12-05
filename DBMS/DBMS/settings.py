aead92e829324fd5920c16b7ba0e23dab3e36461
Ezharuddin-Jubaer <2120117@iub.edu.bd>
2022-12-05 23:50:38 +0600

    update

--- DBMS/DBMS/settings.py
+++ DBMS/DBMS/settings.py
@@ -128,8 +128,7 @@ STATICFILES_DIRS = [BASE_DIR /'static',]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-<<<<<<< Updated upstream
-=======
+
 
 AUTH_USER_MODEL = "user.user"
->>>>>>> Stashed changes
+

35e77dd24adcc00c398f52eb501cbc810f3b353a
Ezharuddin-Jubaer <2120117@iub.edu.bd>
2022-12-05 20:22:56 +0600

    update

--- CMAS/CMAS/settings.py
+++ CMAS/CMAS/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CMAS'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR /'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

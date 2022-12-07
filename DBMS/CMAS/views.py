2ead49535b9cf9ca0a7b2e5788c549dcba9ff1a0
Sumaiya-Karim-Katha <114346589+Sumaiya-Karim-Katha@users.noreply.github.com>
2022-12-07 09:08:55 +0600

    update

--- DBMS/CMAS/views.py
+++ DBMS/CMAS/views.py
@@ -51,26 +51,7 @@ def viewCo(request):
 
 def createCo(request):
     if request.method == 'POST':
-        
-<<<<<<< HEAD
-        Deptname = request.POST['dept']
-        Schoolname = request.POST['school']
-        Coursecode = request.POST['COID']
-        Coursetitle = request.POST['Ctitle']
-        Coursetype = request.POST['Ctype']
-        Courseprereq = request.POST['Cprerequisite']
-        Creditvalue = request.POST['creditvalue']
-        Contacthour_week = request.POST['contact']
-        Coursedescription = request.POST['Cdescription']
-        Courseobjective = request.POST['CObjective']
-        Coursecontent = request.POST['CContent']
-        Assessmenttype = request.POST['atype1']
-        Referencebook = request.POST['reference']
 
-        new_CourseOut = CourseOutline(deptname = Deptname, schoolname = Schoolname, coursecode = Coursecode, coursetitle = Coursetitle, coursetype = Coursetype, courseprereq = Courseprereq, 
-        creditvalue = Creditvalue, contacthour_week = Contacthour_week, coursedescription = Coursedescription, courseobjective = Courseobjective, coursecontent = Coursecontent,
-        assessmenttype = Assessmenttype, referencebook = Referencebook)
-=======
         deptname = request.POST['dept']
         schoolname = request.POST['school']
         coursecode = request.POST['COID']
@@ -88,7 +69,7 @@ def createCo(request):
         new_CourseOut = CourseOutline(deptname = deptname, schoolname = schoolname, coursecode = coursecode, coursetitle = coursetitle, coursetype = coursetype, courseprereq = courseprereq, 
         creditvalue = creditvalue, contacthour_week = contacthour_week, coursedescription = coursedescription, courseobjective = courseobjective, coursecontent = coursecontent,
         assessmenttype = assessmenttype, referencebook = referencebook)
->>>>>>> 4d561dd72c77e1b6f17d5591d478551ac29c981e
+
 
         new_CourseOut.save()
         
@@ -101,25 +82,10 @@ def createCo(request):
         ploassessed = request.POST['plo1']
         copocorrelation = request.POST['CP1']
 
-<<<<<<< HEAD
-=======
-        clonum = request.POST['clo1']
-        clodescription = request.POST['clo1desc']
-        bloomc = request.POST['clo1C']
-        bloomp = request.POST['clo1P']
-        blooma = request.POST['clo1A']
-        ploassessed = request.POST['plo1']
-        copocorrelation = request.POST['CP1']
-
->>>>>>> 4d561dd72c77e1b6f17d5591d478551ac29c981e
         new_Clo = Clo(clonum = clonum, clodescription = clodescription,
                         bloomc = bloomc, bloomp = bloomp, blooma = blooma, ploid=ploassessed, copocorrelation=copocorrelation)
 
         new_Clo.save()
-<<<<<<< HEAD
-=======
-        # return render(request, 'dashboard/faculty/courseoutline/createCO.html')
->>>>>>> 4d561dd72c77e1b6f17d5591d478551ac29c981e
 
     return render(request, 'dashboard/faculty/courseoutline/createCO.html', {"Department":display_dept, "School":display_school, "Course":display_course, "PLO":display_PLO})
 
